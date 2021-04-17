d78fffcf843cc38bc0138fda4a77815630e4dc5e
betlyves <betlyves20@gmail.com>
2021-04-18 01:48:13 +0530

    invoke commit

--- BabyBumpTrial.py
+++ BabyBumpTrial.py
@@ -1,7 +1,11 @@
+import boto3
+ddb = boto3.client("dynamodb")
 import ask_sdk_core
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.dispatch_components import AbstractRequestHandler, AbstractExceptionHandler
 from ask_sdk_core.utils import is_request_type, is_intent_name
+from ask_sdk_model import Response
+
 sb = SkillBuilder()
 
 
@@ -11,10 +15,13 @@ class LaunchRequestHandler(AbstractRequestHandler):
         return is_request_type("LaunchRequest")(handler_input)
     
     def handle(self, handler_input):
-        speech_text = "Welcome to Baby Bump! I'm Lisa";
-        handler_input.response_builder.speak(speech_text).set_should_end_session(False)
-        return handler_input.response_builder.response
-
+        speech_text = "Welcome to Baby Bump! I'm Lisa. When was your last menstrual period?"
+        return (
+            handler_input.response_builder
+                .speak(speech_text)
+                .set_should_end_session(False)
+                .response
+        )
 
 
 class ErrorHandler(AbstractExceptionHandler):
@@ -33,10 +40,16 @@ class BabyBumpHandler(AbstractRequestHandler):
         return is_intent_name("BabyBump")(handler_input)
 
     def handle(self, handler_input):
-        #slotValue = handler_input.request_envelope.request.intent.slots['slotName'].value
-        speech_text = "This is my custom intent handler";
+        date = handler_input.request_envelope.request.intent.slots['date'].value
+        month = handler_input.request_envelope.request.intent.slots['month'].value
+        year = handler_input.request_envelope.request.intent.slots['year'].value
+        #weeks= calculator(date, month, year)
+        speech_text = "Okay so your last period was on {month} {date} {year}".format(month=month, date=date, year=year);
         handler_input.response_builder.speak(speech_text).set_should_end_session(False)
         return handler_input.response_builder.response
+
+    #def calculator(date, month, year):
+
 
 
 
